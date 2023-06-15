06f081a54895dc8a7e0296abfb629b8809dafe6c
prado.cast <priscila.prado@castgroup.com.br>
2023-06-15 18:32:19 -0300

    corrigido

---
+++ main.js
@@ -0,0 +1,14 @@
+let textoA = "OLÁ MUNDO";   
+
+let textoB = "SOMOS A ACADEMY TELECOM";
+
+let frase1 = textoA +"SEJA BEM VINDO" + textoB;
+
+let unir = textoA + textoB 
+
+
+console.log(unir)
+
+
+
+
